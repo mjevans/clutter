121f9417b21c1615830db49bed2743a71f8d45cd
Michael J. Evans <mjevans1983@gmail.com>
2016-07-29 01:19:42 -0700

    uploadFileAutoMethod : Split out the automatic large file logic from uploadFile make the default notify 128MB (binary) make default notify and verbose consistant within the b2 object and it's used objects. clean up all tabs (easiest leading tabs solution), and trailing spaces.

--- b2/pyb2push.py
+++ b2/pyb2push.py
@@ -22,8 +22,8 @@ import datetime
 
 
 class RangeLimiter(object):
-            def __init__(self, path, offset, limit, notify = 100 * 1024 * 1024, verbose = 0, growingFileRace = False):
-                self.fh = open(path, 'rb', buffering=1*1024*1024) # Try reading ahead in case the underlying device has issues.
+            def __init__(self, path, offset, limit, notify = 128 * 1024 * 1024, verbose = 0, growingFileRace = False):
+                self.fh = open(path, 'rb') # default , buffering=1*1024*1024) # Try reading ahead in case the underlying device has issues.
                 self.fh.seek(offset, 0)
                 self.offset = offset
                 #self.fh = os.open(path, os.O_RDONLY|os.O_NONBLOCK)
@@ -81,9 +81,9 @@ class RangeLimiter(object):
 
 class StatefulException(Exception):
     def __init__(self, value = ""):
-	self.value = value
+        self.value = value
     def __str(self):
-	return repr(self.value)
+        return repr(self.value)
 
 
 class b2:
@@ -99,9 +99,11 @@ class b2:
         self.largeFileChunk = 1 * 1024 * 1024 * 1024 # 512 * 1024 * 1024 # 4 * 1024 * 1024 * 1024
         self.minFileChunk =  100000000 # default B2 minimum; may be superceeded by b2_authorize_account
         self.maxFileChunk = 5000000000 # default B2 maximum; may be superceeded by b2_authorize_account
+        self.maxChunkCount = 10000
         self.retryDefault = 8 # seconds, default for start of exponential backoff.
         self.timeout = 35
         self.triesDefault = 5
+        self.uploadNotifyEach = 128 * 1024 * 1024
         self.verbose = 3
 
         # resources
@@ -212,8 +214,8 @@ class b2:
 
     # On BlockingIOError abort operational state; optional: retry from base state
     def postB2(self, postUrl, data, session = None, stateful = False, timeout = None, tries = 5, retryDefault = None):
-	if session is None:
-	    session = self.s
+        if session is None:
+            session = self.s
         if timeout is None:
             timeout = self.timeout
         if tries is None:
@@ -262,10 +264,10 @@ class b2:
                                     "WARNING: Forcing enumeration of buckets.", file=sys.stderr)
                             self.listBuckets()
                             raise BlockingIOError()
-			elif 'bad_request' == robj['code']:
-			    raise BlockingIOError()
-			elif 'file_not_present' == robj['code']
-			    return robj
+                        elif 'bad_request' == robj['code']:
+                            raise BlockingIOError()
+                        elif 'file_not_present' == robj['code']
+                            return robj
                 elif (500 <= r.status_code and r.status_code <= 599):
                     print("POST INFO {}: holding for 900 seconds ({} tries remain)\n\tStatus {}: {}\n\n".format(
                         path, tries, r.status_code, r.text), file=sys.stderr)
@@ -283,7 +285,7 @@ class b2:
                 time.sleep(retry)
                 ## self.authorizeAccount() # do not handle PermissionError
 
-	    raise BlockingIOError() # Out of attempts.
+            raise BlockingIOError() # Out of attempts.
 
 
 
@@ -383,7 +385,7 @@ class b2:
             'fileName': fileName,
             'fileId': fileId
         }
-	ref = self.postAsJSON('/b2api/v1/b2_delete_file_version', req)
+        ref = self.postAsJSON('/b2api/v1/b2_delete_file_version', req)
         self.removeFileNameId(ref['fileName'], ref['fileId'])
         return ref
 
@@ -401,102 +403,44 @@ class b2:
         _file = self.lookupFile(path,None)
         stats = os.stat(path)
         if _file is None:
-            if   (stats.st_size % self.largeFileChunk >= self.minFileChunk and
-                  stats.st_size < self.largeFileChunk * 10000):
-                fileChunk = self.largeFileChunk
-            elif stats.st_size / self.largeFileChunk < 10000:
-                fileChunk = self.largeFileChunk
+            if  stats.st_size > self.maxFileChunk:
+                raise RuntimeError("Use uploadFileAutoMethod to prevent trying to upload a file larger than {} with the small-file upload method. ({} is {})".format(self.maxFileChunk, path, stats.st_size))
             else:
-                fileChunk = self.maxFileChunk
+                fileChunk = stats.st_size + 1
 
             info = digestparallel.digest(path, sha1each = fileChunk)
-            info['fileChunk'] = fileChunk
+            info['fileChunk'] = stats.st_size
         else:
-            info = _file
+            return None
         info['size'] = stats.st_size
         info['mtimens'] = stats.st_mtime_ns
         info['ctimens'] = stats.st_ctime_ns
 
-        if stats.st_size > info['fileChunk']:
-            if self.verbose >= 1:
-                print("uploadFile: Large {}/{}".format(bucket, path), file=sys.stderr)
-            if "largeFileState" not in info:
-                bfile = self.startLargeFile(bucket, path, info)
-                info["largeFileObj"] = bfile
-                info["largeFileState"] = "Started"
-                info["fileId"] = bfile["fileId"]
-                self.storeFile(path, bfile, info)
-                if self.verbose >= 2:
-                    print("Started: {}/{} as {}".format(bucket, path, info["fileId"]),file=sys.stderr)
-            elif "Complete" == info["largeFileState"]:
-                if self.verbose >= 1:
-                    print("File appears to be complete: {}/{} as {}".format(bucket, path, info["fileId"]), file=sys.stderr)
-                return None
-            else:
-                bfile = info["largeFileObj"]
-                if self.verbose >= 2:
-                    print("Attempting to resume: {}/{} as {}".format(bucket, path, info["fileId"]), file=sys.stderr)
-            skiplist = []
-            if "uploaded" not in info:
-                info["uploaded"] = []
-            else:
-                for oo in info["uploaded"]:
-                    skiplist.append(oo["contentSha1"])
-            pfile = self.getUploadPartURL(info["fileId"])
-            for s_part, s_sha1 in enumerate(info["sha1each"]):
-                if s_sha1 in skiplist:
-                    print("Skipping previously uploaded section {} :: {}".format(s_part, s_sha1), file=sys.stderr)
-                    continue
-                tries = 3
-                while tries > 0:
-                    tries -= 1
-                    if self.verbose >= 1:
-                        print("uploadPart: {} tries remain: {} of Large {}/{}".format(tries, s_part, bucket, path), file=sys.stderr)
-                        sys.stderr.flush()
-                    try:
-                        info["uploaded"].append(
-                            self.uploadPart(path, info, s_part, s_sha1, pfile = pfile)
-                        )
-                        skiplist.append(s_sha1)
-                        info["largeFileState"] = str(len(skiplist))
-                        self.storeFile(path, bfile, info)
-                        break
-                    except (StatefulException, ) as e:
-                        pfile = self.getUploadPartURL(info["fileId"])
-                else:
-                    raise RuntimeError("ERROR: Tries exceeded while uploading large file part.")
-            bfile = self.finishLargeFile(bfile["fileId"], info["sha1each"])
-            info["largeFileState"] = "Complete"
-            return self.storeFile(path, bfile, info)
-
-        else:
-            # Use classic single file method
-            if _file:
-                return None
-            while True:
-		bfile = self.getUploadURL(bucket)
-		headers = {
-		    'Authorization': bfile['authorizationToken'],
-		    'X-Bz-File-Name': path.replace(os.sep, '/'), # ??? https://www.backblaze.com/b2/docs/string_encoding.html ??? Python should work by default?
-		    'Content-Type': 'b2/x-auto',
-		    'Contnet-Length': info['size'],  # 'requests' MIGHT update this... but we already have it and that was /might/
-		    'X-Bz-Content-Sha1': info['sha1'],
-		    'X-Bz-Info-src_last_modified_millis': int(info['mtimens'] / 1000.0),
-		    'X-Bz-Info-md5': info['md5'],
-		    'X-Bz-Info-sha256': info['sha256'],
-		    'X-Bz-Info-sha512': info['sha512']
-		}
-		ups = requests.Session()
-		ups.headers.update(headers)
-		with open(path, 'rb') as f:
-		    try:
-			bfile = postB2(bfile["uploadUrl"], f, session = ups, stateful = True, timeout = None)
-			if info['sha1'] != bfile["contentSha1"]:
-			    raise BlockingIOError()
-			self.storeFile(path, bfile, info)
-			return bfile
-		    except (StatefulException,) as e:
-			pass
+        # classic single file method
+        while True:
+            bfile = self.getUploadURL(bucket)
+            headers = {
+                'Authorization': bfile['authorizationToken'],
+                'X-Bz-File-Name': path.replace(os.sep, '/'), # ??? https://www.backblaze.com/b2/docs/string_encoding.html ??? Python should work by default?
+                'Content-Type': 'b2/x-auto',
+                'Contnet-Length': info['size'],  # 'requests' MIGHT update this... but we already have it and that was /might/
+                'X-Bz-Content-Sha1': info['sha1'],
+                'X-Bz-Info-src_last_modified_millis': int(info['mtimens'] / 1000.0),
+                'X-Bz-Info-md5': info['md5'],
+                'X-Bz-Info-sha256': info['sha256'],
+                'X-Bz-Info-sha512': info['sha512']
+            }
+            ups = requests.Session()
+            ups.headers.update(headers)
+            with open(path, 'rb') as f:
+                try:
+                    bfile = postB2(bfile["uploadUrl"], f, session = ups, stateful = True, timeout = None)
+                    if info['sha1'] != bfile["contentSha1"]:
+                        raise BlockingIOError()
+                    self.storeFile(path, bfile, info)
+                    return bfile
+                except (StatefulException,) as e:
+                    continue # up to the while True: loop above self.getUploadPartURL
 
 
     # b2_start_large_file
@@ -531,9 +475,9 @@ class b2:
             'Authorization': pfile['authorizationToken'],
             'X-Bz-Part-Number': s_part + 1,
             'Contnet-Length': \
-                self.largeFileChunk \
+                info['fileChunk'] \
                 if s_part + 1 < len(info["sha1each"]) else \
-                info["size"] % self.largeFileChunk,
+                info["size"] % info['fileChunk'],
             'X-Bz-Content-Sha1': s_sha1
             }
         ups = requests.Session()
@@ -541,14 +485,14 @@ class b2:
         f = RangeLimiter(path,
                             s_part * info['fileChunk'],
                             info['fileChunk'],
-                            notify = 20 * 1024 * 1024,
-                            verbose = 1)
-			# On BlockingIOError abort operational state; optional: retry from base state
-			def postB2(self, postUrl, data, session = None, timeout = None, tries = 5, retryDefault = None):
-	robj = postB2(pfile["uploadUrl"], f, session = ups, stateful = True, timeout = None)
-	if s_sha1 != robj["contentSha1"]:
-	    raise BlockingIOError()
-	return robj
+                            notify = self.uploadNotifyEach,
+                            verbose = self.verbose)
+                        # On BlockingIOError abort operational state; optional: retry from base state
+                        def postB2(self, postUrl, data, session = None, timeout = None, tries = 5, retryDefault = None):
+        robj = postB2(pfile["uploadUrl"], f, session = ups, stateful = True, timeout = None)
+        if s_sha1 != robj["contentSha1"]:
+            raise BlockingIOError()
+        return robj
 
     # b2_cancel_large_file
     def cancelLargeFile(self, fileID):
@@ -558,6 +502,82 @@ class b2:
     def finishLargeFile(self, fileID, sha1each):
         return self.postAsJSON('/b2api/v1/b2_finish_large_file',
                                 { "fileId": fileID, "partSha1Array": sha1each})
+
+
+    def uploadFileAutoMethod(self, bucket, path, info = None):
+        _file = self.lookupFile(path,None)
+        stats = os.stat(path)
+        if _file is None:
+            if   stats.st_size / self.largeFileChunk < self.maxChunkCount:
+                fileChunk = self.largeFileChunk
+            # Else, try the largest multiple that's less than the maxFileChunk
+            elif stats.st_size / (self.maxFileChunk // self.largeFileChunk * self.largeFileChunk) <= self.maxChunkCount:
+                fileChunk = self.maxFileChunk // self.largeFileChunk * self.largeFileChunk
+            elif stats.st_size / self.maxFileChunk <= self.maxChunkCount:
+                fileChunk = self.maxFileChunk
+            else:
+                raise RuntimeError("Too large: {} has a reported size of {} which is larger than the maximum size {} ({} * {})".format(
+                    path, stat.st_size, self.maxChunkCount * stat.st_size, stat.st_size, self.maxChunkCount))
+
+            info = digestparallel.digest(path, sha1each = fileChunk)
+            info['fileChunk'] = fileChunk
+        else:
+            info = _file
+        info['size'] = stats.st_size
+        info['mtimens'] = stats.st_mtime_ns
+        info['ctimens'] = stats.st_ctime_ns
+
+        if stats.st_size > info['fileChunk']:
+            if self.verbose >= 1:
+                print("uploadFile: Large {}/{}".format(bucket, path), file=sys.stderr)
+            if "largeFileState" not in info:
+                bfile = self.startLargeFile(bucket, path, info)
+                info["largeFileObj"] = bfile
+                info["largeFileState"] = "Started"
+                info["fileId"] = bfile["fileId"]
+                self.storeFile(path, bfile, info)
+                if self.verbose >= 2:
+                    print("Started: {}/{} as {}".format(bucket, path, info["fileId"]),file=sys.stderr)
+            elif "Complete" == info["largeFileState"]:
+                if self.verbose >= 1:
+                    print("File appears to be complete: {}/{} as {}".format(bucket, path, info["fileId"]), file=sys.stderr)
+                return None
+            else:
+                bfile = info["largeFileObj"]
+                if self.verbose >= 2:
+                    print("Attempting to resume: {}/{} as {}".format(bucket, path, info["fileId"]), file=sys.stderr)
+            skiplist = []
+            if "uploaded" not in info:
+                info["uploaded"] = []
+            else:
+                for oo in info["uploaded"]:
+                    skiplist.append(oo["contentSha1"])
+            while True:
+                pfile = self.getUploadPartURL(info["fileId"])
+                try:
+                    for s_part, s_sha1 in enumerate(info["sha1each"]):
+                        if s_sha1 in skiplist:
+                            print("Skipping previously uploaded section {} :: {}".format(s_part, s_sha1), file=sys.stderr)
+                            continue # Next part
+                        info["uploaded"].append(
+                            self.uploadPart(path, info, s_part, s_sha1, pfile = pfile)
+                        )
+                        skiplist.append(s_sha1)
+                        self.storeFile(path, bfile, info)
+                    break # loop: while True
+                except(StatefulException, ) as e:
+                    continue # to try again : go up to the while True: loop above self.getUploadPartURL
+            bfile = self.finishLargeFile(bfile["fileId"], info["sha1each"])
+            info["largeFileState"] = "Complete"
+            return self.storeFile(path, bfile, info)
+
+        else:
+            # Use classic single file method
+            if _file:
+                return None
+            return self.uploadFile(bucket, path, info)
+
+
 
 def simpleExample(bucket_files):
     bb = b2()
@@ -572,7 +592,7 @@ def simpleExample(bucket_files):
         os.chdir(bucket)
         if os.path.exists(rpath):
             print("{} :: {}".format(bucket, rpath), file=sys.stderr)
-            bb.uploadFile(bucket, rpath)
+            bb.uploadFileAutoMethod(bucket, rpath)
         os.chdir(cwd)
 
 if __name__ == "__main__":
@@ -618,7 +638,7 @@ Parts start at 1 (Q: 0 is the whole file?)
 The sha1 checksum of each segment must be specified for that segment, an sha1 of the whole file is optional (recommended).
 
 sed -i 's/^[[:space:]]\+$//'
-sed -i 's/[[:space:]]\+$//'
+sed -i 's/\t/        /g;s/[[:space:]]\+$//'
 
 
 """
